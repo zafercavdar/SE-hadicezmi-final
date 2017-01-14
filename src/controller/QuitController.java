790ac2fd6d956f2c69d9c04dda3416d6b16968a3
zafercavdar <zafercavdar@yahoo.com>
2017-01-14 05:08:32 +0300

    demo version

---
+++ src/controller/QuitController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class QuitController {
+
+	/**
+	 * @requires Game to be started.
+	 * @effects Quits the game.
+	 */
+	public void buttonClick() {
+		System.exit(0);
+	}
+}
